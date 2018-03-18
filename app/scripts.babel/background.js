92b93a82400569be28216297d9a1aa9bcc6358a0
Jalal Mostafa <jalalmostafa22@gmail.com>
2018-03-18 22:55:08 +0200

    adapt new provider and notifications in background

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -17,6 +17,22 @@ let onInstallUpdate = function (msg, header) {
     });
 };
 
+chrome.runtime.onInstalled.addListener(function (details) {
+    let i18n = new I18nService();
+    if (details.reason) {
+        switch (details.reason) {
+            case 'install':
+                onInstallUpdate(i18n.notificationsMessage.title + 'Dev.', i18n.header.title);
+                break;
+            case 'update':
+                // onInstallUpdate(i18n.fixMessage.title, i18n.header.title);
+                break;
+            default:
+                break;
+        }
+    }
+});
+
 let data = new DataService();
 let i18n = new I18nService();
 let alarms = new AlarmService(function (alarm) {
@@ -30,51 +46,32 @@ let alarms = new AlarmService(function (alarm) {
     });
 });
 let run = function () {
-    data.times().then(function (times) {
-        console.log('times is', times);
-        data.notify().then(function (notify) {
-            console.log('notify is', notify);
-            if (notify) {
-                let now = moment();
-                console.log('now', now.unix());
-                console.log('fajr:', times.fajr);
-                console.log('shrouk:', times.shrouk);
-                console.log('dhor:', times.dhor);
-                console.log('maghreb:', times.maghreb);
-
-                if (!now.isAfter(times.fajr)) {
-                    alarms.create('fajrAlarm', moment(times.fajr).unix() * 1000, i18n.fajr.title, i18n.fajr.message);
-                }
-                if (!now.isAfter(times.shrouk)) {
-                    alarms.create('shroukAlarm', moment(times.shrouk).unix() * 1000, i18n.shrouk.title, i18n.shrouk.message);
-                }
-                if (!now.isAfter(times.dhor)) {
-                    alarms.create('dhorAlarm', moment(times.dhor).unix() * 1000, i18n.dhor.title, i18n.dhor.message);
-                }
-                if (!now.isAfter(times.maghreb)) {
-                    alarms.create('maghrebAlarm', moment(times.maghreb).unix() * 1000, i18n.maghreb.title, i18n.maghreb.message);
-                }
-            }
-        });
-    });
-
-    chrome.runtime.onInstalled.addListener(function (details) {
-        let i18n = new I18nService();
-        if (details.reason) {
-            switch (details.reason) {
-                case 'install':
-                    onInstallUpdate(i18n.notificationsMessage.title + 'Dev.', i18n.header.title);
-                    break;
-                case 'update':
-                    // onInstallUpdate(i18n.fixMessage.title, i18n.header.title);
-                    break;
-                default:
-                    break;
-            }
+    $.when(data.times(), data.notifications(), data.hourFormat()).then(function (times, notifications, format) {
+        format = format ? 'h:mm' : 'HH:mm';
+        let now = moment();
+        if (!now.isAfter(moment(times.fajr, format)) && notifications.fajr) {
+            alarms.create('fajrAlarm', moment(times.fajr).unix() * 1000, i18n.fajr.title, i18n.fajr.message);
+        }
+        if (!now.isAfter(moment(times.imsak, format)) && notifications.imsak) {
+            alarms.create('imsakAlarm', moment(times.imsak).unix() * 1000, i18n.imsak.title, i18n.imsak.message);
+        }
+        if (!now.isAfter(moment(times.sunrise, format)) && notifications.sunrise) {
+            alarms.create('shroukAlarm', moment(times.sunrise).unix() * 1000, i18n.sunrise.title, i18n.sunrise.message);
+        }
+        if (!now.isAfter(moment(times.dhuhr, format)) && notifications.dhuhr) {
+            alarms.create('dhuhrAlarm', moment(times.dhuhr).unix() * 1000, i18n.dhuhr.title, i18n.dhuhr.message);
+        }
+        if (!now.isAfter(moment(times.asr, format)) && notifications.asr) {
+            alarms.create('asrAlarm', moment(times.asr).unix() * 1000, i18n.asr.title, i18n.asr.message);
+        }
+        if (!now.isAfter(moment(times.maghrib, format)) && notifications.maghrib) {
+            alarms.create('maghribAlarm', moment(times.maghrib).unix() * 1000, i18n.maghrib.title, i18n.maghrib.message);
+        }
+        if (!now.isAfter(moment(times.isha, format)) && notifications.isha) {
+            alarms.create('maghrebAlarm', moment(times.isha).unix() * 1000, i18n.isha.title, i18n.isha.message);
         }
     });
     let tomorrow = Math.ceil((moment().add(1, 'days').startOf('day').unix() - moment().unix()) * 1e3);
-    console.log(tomorrow, moment().add(1, 'days').startOf('day').unix());
     setTimeout(run, tomorrow);
 };
 
